57028a503412d90fe448f5a17a16c7844a220c88
Naman8042 <131790382+Naman8042@users.noreply.github.com>
2025-09-05 23:23:35 +0530

    Any removed

--- app/_components/signup-form.tsx
+++ app/_components/signup-form.tsx
@@ -6,7 +6,7 @@ import { Label } from "@/components/ui/label";
 import { useEffect, useState } from "react";
 import { useAccount, useWriteContract } from "wagmi";
 import { CONTRACT_ABI, CONTRACT_ADDRESS } from "@/lib/contract";
-import axios from 'axios'
+import axios ,{AxiosError} from 'axios'
 
 export function LoginForm({
   className,
@@ -32,9 +32,11 @@ export function LoginForm({
 
 const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
+
+
 const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault();
-  setErrorMessage(null); // reset
+  setErrorMessage(null);
 
   try {
     const res = await axios.post("/api/signup", {
@@ -45,7 +47,6 @@ const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
 
     console.log("✅ User saved in DB:", res.data);
 
-    // Call smart contract after success
     const txs = writeContract({
       abi: CONTRACT_ABI,
       address: CONTRACT_ADDRESS,
@@ -54,19 +55,24 @@ const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
     });
 
     console.log("✅ Contract transaction sent:", txs);
-  } catch (err: any) {
-    console.error("❌ Registration failed:", err);
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      console.error("❌ Axios error:", err);
 
-    // Show API error message if available
-    if (err.response?.status === 409) {
-      setErrorMessage("Email or Wallet Address already registered");
-    } else if (err.response?.data?.error) {
-      setErrorMessage(err.response.data.error);
+      if (err.response?.status === 409) {
+        setErrorMessage("Email or Wallet Address already registered");
+      } else if (err.response?.data?.error) {
+        setErrorMessage(err.response.data.error);
+      } else {
+        setErrorMessage("Something went wrong. Please try again.");
+      }
     } else {
-      setErrorMessage("Something went wrong. Please try again.");
+      console.error("❌ Unknown error:", err);
+      setErrorMessage("Unexpected error occurred");
     }
   }
 };
+
 
 
 
